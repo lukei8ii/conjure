bfb830fa9439295431084058e42d17828a9ffa60
Shea Hunter <shea.hunter@gmail.com>
2020-08-28 16:21:51 -0500

    added some images

--- util.js
+++ util.js
@@ -14,3 +14,69 @@ $("table#example tr.odd, table#example tr.even").each((index, element) => {
         success: (data) => { monsterList[index].html = $(data).filter("div.container").first().html(); }
     });
 })
+
+
+// https://sword-coast-legends.fandom.com/wiki/Giant_Wolf_Spider
+// https://forgottenrealms.fandom.com/wiki/Langdedrosa_Cyanwrath
+// https://www.dndbeyond.com/monsters/riding-horse
+// https://www.aidedd.org/dnd/monstres.php?vo=aarakocra
+
+[
+    { url: "https://sword-coast-legends.fandom.com/wiki/", selector: "#mw-content-text > table.sclinfobox > tbody > tr:nth-child(2) > td > a > img", format: "snakeAndTitle" },
+    { url: "https://forgottenrealms.fandom.com/wiki/", selector: "#pi-tab-0 > figure > a > img", format: "snakeAndTitle" },
+    { url: "https://www.aidedd.org/dnd/monstres.php?vo=", selector: "div.bloc > div.picture > amp-img > img", format: "kebab" },
+    { url: "https://www.dndbeyond.com/monsters/", selector: "#content > section > div > div.more-info.details-more-info > aside > div > a > img", format: "kebab" }
+]
+
+monsterList.forEach((monster, index) => {
+    let creatureName = monster.name.replace(/ /g, "_");
+    let imageUrl = `https://sword-coast-legends.fandom.com/wiki/${creatureName}`;
+
+    $.get(imageUrl, (data) => { 
+        let image = $(data).find("#mw-content-text > table.sclinfobox > tbody > tr:nth-child(2) > td > a > img").attr("src");
+        monsterList[index].image = image;
+    })
+    .fail(function(error) { 
+        console.log(error);
+    });
+})
+
+monsterList.forEach((monster, index) => {
+    let creatureName = monster.name.replace(/ /g, "_");
+    let imageUrl = `https://forgottenrealms.fandom.com/wiki/${creatureName}`;
+
+    $.get(imageUrl, (data) => { 
+        let image = $(data).find("#pi-tab-0 > figure > a > img").attr("src");
+        monsterList[index].image = monsterList[index].image || image;
+    })
+    .fail(function(error) { 
+        console.log(error);
+    });
+})
+
+monsterList.forEach((monster, index) => {
+    let creatureName = monster.name.toLowerCase().replace(/ /g, "-");
+    let imageUrl = `https://www.aidedd.org/dnd/monstres.php?vo=${creatureName}`;
+
+    $.get(imageUrl, (data) => { 
+        dataStuff = data;
+        let image = $(data).filter("div.bloc").find("div.picture amp-img").attr("src");
+        monsterList[index].image = monsterList[index].image || image;
+    })
+    .fail(function(error) { 
+        console.log(error);
+    });
+})
+
+monsterList.forEach((monster, index) => {
+    let creatureName = monster.name.toLowerCase().replace(/ /g, "-");
+    let imageUrl = `https://www.dndbeyond.com/monsters/${creatureName}`;
+
+    $.get(imageUrl, (data) => { 
+        let image = $(data).find("#content > section > div > div.more-info.details-more-info > aside > div > a > img").attr("src");
+        monsterList[index].image = monsterList[index].image || image;
+    })
+    .fail(function(error) { 
+        console.log(error);
+    });
+})
